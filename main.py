ddf38e0a62e24ab67ffc86461a4cf6993c329bd6
Joshua Wright <40744027+Jwright707@users.noreply.github.com>
2020-12-29 13:49:36 -0800

    Short term memory, comments, and word_cleaner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import re
 
 import nltk
 import numpy as np
@@ -13,6 +14,29 @@ stemmer = LancasterStemmer()
 with open("intents.json") as file:
     data = json.load(file)
 
+
+# --------------Preprocessing the data (Natural Language Processing) ---------------------------------------------------
+
+
+def word_cleaner(string):
+    string = string.lower()
+    string = re.sub(r"i'm", "i am", string)
+    string = re.sub(r"it's", "it is", string)
+    string = re.sub(r"he's", "he is", string)
+    string = re.sub(r"she's", "she is", string)
+    string = re.sub(r"that's", "that is", string)
+    string = re.sub(r"what's", "what is", string)
+    string = re.sub(r"where's", "where is", string)
+    string = re.sub(r"\'ll", " will", string)
+    string = re.sub(r"\'ve", " have", string)
+    string = re.sub(r"\'re", " are", string)
+    string = re.sub(r"\'d", " would", string)
+    string = re.sub(r"won't", "will not", string)
+    string = re.sub(r"can't", "cannot", string)
+    string = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", string)
+    return string
+
+
 try:
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
@@ -21,12 +45,12 @@ except:
     labels = []
     docs_x = []
     docs_y = []
-
+    ignore_characters = ['!', '?', ',', '.']
     for intent in data["intents"]:
         for patten in intent["patterns"]:
             # stemmer takes each word in a pattern and reduces it to its root word
             # there? -> there or whats up -> what
-            tokenized_words = nltk.word_tokenize(patten)
+            tokenized_words = nltk.word_tokenize(word_cleaner(patten))
             words.extend(tokenized_words)
             docs_x.append(tokenized_words)
             docs_y.append(intent["tag"])
@@ -35,7 +59,7 @@ except:
             labels.append(intent["tag"])
 
     # removes any duplicates
-    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+    words = [stemmer.stem(w.lower()) for w in words if w not in ignore_characters]
     words = sorted(list(set(words)))
 
     labels = sorted(labels)
@@ -68,8 +92,11 @@ except:
 
     with open("data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output), f)
+# ----------------------------------------------------------------------------------------------------------------------
 
 tf.reset_default_graph()
+
+# --------------Creating the Neural network layers (Deep Learning) -----------------------------------------------------
 
 # Input data which is the len of the training data
 net = tflearn.input_data(shape=[None, len(training[0])])
@@ -79,12 +106,15 @@ net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
 # Have 8 neurons for the output layer (softmax gives a probability for each layer)
 net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-
 net = tflearn.regression(net)
 # DNN is a type of neural network
 model = tflearn.DNN(net)
 model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 model.save("model.tflearn")
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# There is an error occuring here, this is temporary commented out
 # try:
 #     model.load("model.tflearn")
 # except:
@@ -108,21 +138,27 @@ def bag_of_words(user_input, words):
 
 def chat():
     print("Bot is ready to talk! (quit to stop)")
+    # short term memory
+    context_state = None
     while True:
         user_input = input("You: ")
         if user_input.lower() == "quit":
             break
-
-        results = model.predict([bag_of_words(user_input, words)])[0]
+        results = model.predict([bag_of_words(word_cleaner(user_input), words)])[0]
         # np.argmax gives the index of the greatest value in the list
         results_index = np.argmax(results)
         tag = labels[results_index]
         if results[results_index] > 0.7:
             for tg in data["intents"]:
-
                 if tg['tag'] == tag:
-                    responses = tg['responses']
-                    print(random.choice(responses))
+                    # checks to see if the short term state is being used or not
+                    if 'context_filter' not in tg or 'context_filter' in tg and tg['context_filter'] == context_state:
+                        responses = tg['responses']
+                        if 'context_set' in tg:
+                            context_state = tg['context_set']
+                        else:
+                            context_state = None
+                        print(random.choice(responses))
         else:
             print("I don't understand, try asking a different question.")
 
