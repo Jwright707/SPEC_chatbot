f03cfe3d3a05aa37a148c81d981d1737d937f4e4
Joshua Wright <40744027+Jwright707@users.noreply.github.com>
2021-01-27 12:49:03 -0800

    Responses, questions, and cycling
    Made responses correspone to each question and made it so each Q&A will be put back into the intents file

--- Helpers/unidentified_question_helper.py
+++ Helpers/unidentified_question_helper.py
@@ -1,20 +1,19 @@
-import json
-import os
+import uuid
 
 from Helpers.slack_message_format import unidentified_format
 
 
 def unidentified_question_helper(unidentified_questions, joined_input, response, slack_client):
-    unidentified_questions.append({
-        'tag': "NEED TAG",
-        'response': [joined_input],
-        'patterns': [],
+    question_id = str(uuid.uuid4())
+    unidentified_questions[question_id] = {
+        'tag': question_id,
+        'patterns': [joined_input],
+        'response': [],
         'context_set': ''
-    })
+    }
     response["response"] = "I don't understand, try asking a different question."
     response["context_state"] = ""
-    with open('unidentified_questions.json', 'w') as outfile:
-        json.dump(unidentified_questions, outfile, indent=4)
-    slack_message = unidentified_format(joined_input)
-    slack_client.chat_postMessage(**slack_message)
+    if len(list(unidentified_questions)) == 1:
+        slack_message = unidentified_format(joined_input)
+        slack_client.chat_postMessage(**slack_message)
     return response
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from flask import Flask, request, Response
 from flask_cors import CORS
 
 from FlaskRoutes.route import chatbot_route
-from Helpers.slack_message_format import response_format
+from Helpers.slack_message_format import response_format, unidentified_format
 from Preprocessing.preprocessing_data import preprocessing_data
 from NeuralNetwork.neural_network_layers import neural_network
 from slack import WebClient
@@ -35,12 +35,7 @@ model = neural_network(training, output)
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-try:
-    with open("unidentified_questions.json") as json_file:
-        unidentified_questions = json.load(json_file)
-except FileNotFoundError:
-    unidentified_questions = []
-
+unidentified_questions = {}
 
 @app.route("/", methods=['POST'])
 def chatbot():
@@ -51,8 +46,31 @@ def chatbot():
 def answer():
     answer_request = request.form
     slack_response = answer_request["text"]
-    thank_you_response = response_format(slack_response)
-    slack_client.chat_postMessage(**thank_you_response)
+    questions = unidentified_questions
+    if len(list(questions.keys())) >= 1:
+        first_key = list(questions.keys())[0]
+        unidentified_questions[first_key]['response'].append(slack_response)
+
+        with open("test.json") as json_file:
+            existing_data = json.load(json_file)
+
+        existing_data['intents'].append(unidentified_questions[first_key])
+
+        with open('test.json', 'w') as outfile:
+            json.dump(existing_data, outfile, indent=4)
+
+        del unidentified_questions[first_key]
+        thank_you_response = response_format(slack_response)
+        slack_client.chat_postMessage(**thank_you_response)
+        if len(list(questions.keys())) >= 1:
+            second_key = list(questions.keys())[0]
+            user_question = unidentified_questions[second_key]['patterns'][0]
+            slack_message = unidentified_format(user_question)
+            slack_client.chat_postMessage(**slack_message)
+
+    else:
+        slack_client.chat_postMessage(channel=os.getenv("DOTTY_CHANNEL_ID"),
+                                      text="There are currently no unidentified questions")
     return Response(), 200
 
 
